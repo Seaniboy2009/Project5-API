2277d905f85e20d84fe3ed39fc769a1f245a0047
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-03-27 10:06:45 +0000

    add serializers.py for profiles model view

--- drf_project5_api/profiles/views.py
+++ drf_project5_api/profiles/views.py
@@ -1,11 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from .serializers import ProfileSerializer
 
 class ProfileList(APIView):
     '''
-    Profile list view, gets all of the profiles and lists them
+    Get all profiles and list them
     '''
     def get(self, request):
         profiles = Profile.objects.all()
-        return Response(profiles)
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
