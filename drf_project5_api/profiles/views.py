5988f66037b547c9b44ca9325719270b07d741e1
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-03-27 09:38:45 +0000

    add urls.py for profiles

--- drf_project5_api/profiles/views.py
+++ drf_project5_api/profiles/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
 
-# Create your views here.
+class ProfileList(APIView):
+    '''
+    Profile list view, gets all of the profiles and lists them
+    '''
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)
