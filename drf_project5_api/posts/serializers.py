acb7be94ce63f5a4876ebb82f0c999f54190bbc1
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-03-28 09:17:00 +0000

    add image size validation for posts

--- drf_project5_api/posts/serializers.py
+++ drf_project5_api/posts/serializers.py
@@ -8,6 +8,22 @@ class PostSerializer(serializers.ModelSerializer):
     '''
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image is larger than 2mb, please resize')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px'
+            )
+        return value
+
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -16,8 +32,9 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'title', 'advert_image', 'reality_image',
-            'location', 'franchisor', 'created_at', 'updated_at',
-            'content', 'is_owner'
+            'id', 'owner', 'is_owner', 'title', 'profile_id',
+            'profile_image', 'advert_image', 'reality_image','location', 
+            'franchisor', 'created_at', 'updated_at',
+            'content', 
         ]
 
