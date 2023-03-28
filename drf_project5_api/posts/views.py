6c00edf35ff4793f3eb16fa804d30aa715419acb
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-03-28 09:41:46 +0000

    refactor of posts.views

--- drf_project5_api/posts/views.py
+++ drf_project5_api/posts/views.py
@@ -1,48 +1,28 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from .models import Post
 from .serializers import PostSerializer
 from django.http import Http404
 
 
-class PostList(APIView):
+class PostList(generics.ListCreateAPIView):
     '''
-    Get all posts and list them
+    Get all profiles and list them, create post will
+    assign the creator to the post
     '''
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True, context={ 'request': request })
-        return Response(serializer.data)
-    
-    # def post(self, request):
-    #     serializer = PostSerializer(data=request.data, context)
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class PostDetail(APIView):
     '''
     Get one post by primary key and return
     '''
-    
-    # change the serializer class to be a form for input
     serializer_class = PostSerializer
-
-    def get_post(self, pk):
-        try:
-            post = Post.objects.get(pk=pk)
-            return post
-        except Post.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        post = self.get_post(pk)
-        serializer = PostSerializer(post, context={ 'request': request })
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        post = self.get_post(pk)
-        serializer = PostSerializer(post, data=request.data, context={ 'request': request })
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.all()
