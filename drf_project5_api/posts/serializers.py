c52dc10cef726720ad691d1c63448c37ee63f6d4
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-03-28 12:40:50 +0000

    add get like id for posts serializers

--- drf_project5_api/posts/serializers.py
+++ drf_project5_api/posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from like.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -10,6 +11,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -28,11 +30,23 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    # Get the first like for the logged in user
+    # and the current pos, there should only be 1
+    # per user per post
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'is_owner', 'title', 'profile_id',
+            'id', 'owner', 'is_owner', 'title', 'profile_id', 'like_id',
             'profile_image', 'advert_image', 'reality_image','location', 
             'franchisor', 'created_at', 'updated_at',
             'content', 
