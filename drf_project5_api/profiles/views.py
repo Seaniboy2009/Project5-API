329a3a2274b9f03dac2028a480f86675ef018b06
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-03-28 09:38:41 +0000

    refactor of profiles.views

--- drf_project5_api/profiles/views.py
+++ drf_project5_api/profiles/views.py
@@ -6,54 +6,18 @@ from .serializers import ProfileSerializer
 from django.http import Http404
 from drf_project5_api.permissions import IsOwnerOrReadOnly
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView()):
     '''
     Get all profiles and list them
     '''
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True, context={ 'request': request })
-        return Response(serializer.data)
-
-
-# class ProfileDetail(APIView):
-#     '''
-#     Get one profile by primary key and return
-#     '''
-    
-#     # change the serializer class to be a form for input
-#     serializer_class = ProfileSerializer
-#     # Change the permissions class to use custom
-#     permission_classes = [IsOwnerOrReadOnly]
-
-#     def get_profile(self, pk):
-#         try:
-#             profile = Profile.objects.get(pk=pk)
-#             self.check_object_permissions(self.request, profile)
-#             return profile
-#         except Profile.DoesNotExist:
-#             raise Http404
-    
-#     def get(self, request, pk):
-#         profile = self.get_profile(pk)
-#         serializer = ProfileSerializer(profile, context={ 'request': request })
-#         return Response(serializer.data)
-    
-#     def put(self, request, pk):
-#         profile = self.get_profile(pk)
-#         serializer = ProfileSerializer(profile, data=request.data, context={ 'request': request })
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     '''
-    Get one profile by primary key and return
+    Get one profile and if you are the owner you can edit
     '''
-    # change the serializer class to be a form for input
     serializer_class = ProfileSerializer
-    # Change the permissions class to use custom
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all()
