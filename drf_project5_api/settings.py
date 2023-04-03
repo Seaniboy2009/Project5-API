fdbf1784b6b189423ec38635fd03418332d40f67
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-04-03 12:32:05 +0000

    fix token/logout fix2

--- drf_project5_api/settings.py
+++ drf_project5_api/settings.py
@@ -56,7 +56,7 @@ JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'drf_project5_api.serializers.CurrentUserSerializer'
 }
 
 # Quick-start development settings - unsuitable for production
